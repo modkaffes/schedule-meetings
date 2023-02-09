3553b7bcd8c781d442407da24f370f8ab35b2965
Modestos Kaffes <modkaffes@gmail.com>
2023-02-09 12:46:22 +0100

    Add indication for user type on top right

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { classNames } from "@/utils/classNames";
+
 import Link from "next/link";
+import { useSelectedLayoutSegments } from "next/navigation";
 
 export default function Nav() {
+  const [selectedLayoutSegments] = useSelectedLayoutSegments();
   return (
     <nav className="mx-auto max-w-4xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between">
@@ -11,6 +15,15 @@ export default function Nav() {
             Meeting Scheduler
           </h1>
         </Link>
+        <span
+          className={classNames(
+            selectedLayoutSegments === "organizer" && "text-blue-600",
+            selectedLayoutSegments === "attendee" && "text-green-600",
+            "font-semibold"
+          )}
+        >
+          {selectedLayoutSegments && selectedLayoutSegments.toUpperCase()}
+        </span>
       </div>
     </nav>
   );
