febc68e66b74f35078d295a1355f20b60410f9ad
Modestos Kaffes <modkaffes@gmail.com>
2023-02-09 10:37:15 +0100

    Add Attendee page

---
+++ app/attendee/[attendeeId]/page.tsx
@@ -0,0 +1,29 @@
+async function getUser({ attendeeId }: { attendeeId: string }) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/attendees/${attendeeId}`
+  );
+
+  // TODO: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page({
+  params,
+}: {
+  params: { attendeeId: string };
+}) {
+  const { attendeeId } = params;
+
+  const { name } = await getUser({ attendeeId });
+
+  return (
+    <>
+      <h1 className="text-xl font-semibold text-gray-900">Hello, {name}!</h1>
+    </>
+  );
+}
---
+++ app/attendee/head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+  return (
+    <>
+      <title>Attendee page | Meeting Scheduler</title>
+    </>
+  );
+}
