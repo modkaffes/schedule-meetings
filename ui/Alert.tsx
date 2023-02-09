4d18b34976a78a64f77bc73f9b29f94e9b0201b0
Modestos Kaffes <modkaffes@gmail.com>
2023-02-09 10:16:27 +0100

    Add Alert component to act as Toaster container

---
+++ ui/Alert.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { Toaster } from "react-hot-toast";
+
+// This is just a client component that renders the toaster container.
+export default function Alert() {
+  return <Toaster />;
+}
